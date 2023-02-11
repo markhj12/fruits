271832a076bc070ed93ad52265e01d9f87aa1ec6
markhew <markhewjin@gmail.com>
2023-02-11 14:13:04 +0800

    initial commit

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Mark Hew on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
