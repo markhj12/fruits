c1554dd8e376cfb9c76061558ff8b5e446f6aaf6
markhew <markhewjin@gmail.com>
2023-02-11 15:11:46 +0800

    onboarding

---
+++ Fructus/Fructus/View/StartButtonView.swift
@@ -0,0 +1,41 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Mark Hew on 11/2/23.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    //MARK: PROPERTIES
+    
+    //MARK: BODY
+    var body: some View {
+        Button(action: {
+            print("Exit the onboarding")
+        }) {
+            HStack(spacing: 8) {
+                Text("Start")
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            }//HSTACK
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(
+                Capsule().strokeBorder(Color.white, lineWidth: 1.25)
+            )
+            
+        }//BUTTON
+        .accentColor(Color.white)
+            
+    }
+}
+
+//MARK: PREVIEW
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .previewLayout(.sizeThatFits)
+    }
+}
